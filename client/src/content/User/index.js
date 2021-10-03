b23b0cb8a32000023bedc8ebe6cc19ccd41f95fe
Erich Casagrande <erichfr0zen@gmail.com>
2021-10-02 21:48:27 -0300

    start test elements

--- client/src/content/User/index.js
+++ client/src/content/User/index.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Toast, ToastContainer, Alert } from "react-bootstrap";
 import Header from "../../components/Header";
 function User() {
   return (
     <React.Fragment>
       <Header />
       <Container>
-        <Row>test</Row>
+        <Row>
+          {/* toast*/}
+          <ToastContainer position="bottom-end" className="p-3">
+            <Toast>
+              <Toast.Header>
+                <strong className="me-auto">
+                  Your info is showing in the top here :D{" "}
+                </strong>
+              </Toast.Header>
+              <Toast.Body>
+                Woohoo, you're reading this text in a Toast!
+              </Toast.Body>
+            </Toast>
+          </ToastContainer>
+          {/* Alert */}
+          <Alert variant="danger" onClose={() => null} dismissible>
+            You connection is bad;
+          </Alert>
+        </Row>
       </Container>
     </React.Fragment>
   );
